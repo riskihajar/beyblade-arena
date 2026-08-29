ab090e68583d33544febd69b36949f118153c852
riskihajar <riskihajar@gmail.com>
2026-08-29 08:38:54 +0800

    feat(branding): use Lucide Swords icon for dashboard logo, header, auth pages, and favicon

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -221,7 +221,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                     Navigation Menu
                                 </SheetTitle>
                                 <SheetHeader className="flex justify-start text-left">
-                                    <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
+                                    <AppLogoIcon className="size-6 text-foreground" />
                                 </SheetHeader>
                                 <div className="flex h-full flex-1 flex-col space-y-4 p-4">
                                     <div className="flex h-full flex-col justify-between text-sm">
